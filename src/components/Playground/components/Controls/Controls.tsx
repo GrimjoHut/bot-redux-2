e3702bb554e1af85424909cade9647366e10b017
Bogdan Zotov <zotov.bogdan2017@yandex.ru>
2024-03-08 21:12:20 +0300

    init

--- src/components/Playground/components/Controls/Controls.tsx
+++ src/components/Playground/components/Controls/Controls.tsx
@@ -1,4 +1,6 @@
-// import styles from "./Controls.module.css"
+import "./Controls.css"
+import LockPick from "./../../Resources/Images/LockpickButton.png"
+import { useState } from "react"
 
 export interface IControlsProps {
     isTimerActive: boolean,
@@ -6,11 +8,22 @@ export interface IControlsProps {
 }
 
 const Controls: React.FC<IControlsProps> = (props) => {
+    const [isShowLockpick, setIsShowLockpick] = useState<boolean>(true)
     const { isTimerActive, setIsTimerActive} = props
+    const HandleLockpick = () => {
+        setIsTimerActive(true)
+        setIsShowLockpick(false)
+    }
 
-    return <div>
-        <button onClick={() => setIsTimerActive(true)} disabled={isTimerActive}>Play</button>
+    return <>
+    <span className={isShowLockpick? "ControlsContainerSpan" : "ControlsContainerSpanHidden"}>There are some tools,<br></br>that can help you</span>
+    <div className={isShowLockpick? "ControlsContainer" : "ControlsContainerHidden" }>
+        <img className="Lockpick" src={LockPick} onClick={HandleLockpick}></img>
     </div>
+    <div className={isShowLockpick? "MotivationwWordsHidden" : "MotivationwWords"}>
+        
+    </div>
+    </>
 }
 
 export default Controls
